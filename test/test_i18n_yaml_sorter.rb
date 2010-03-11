b53f9814bc63f3bf0d5cb9864707e8478ae1c1ce
Bernardo de Pádua <berpasan@gmail.com>
2010-03-11 12:31:39 -0300

    Initial commit to i18n_yaml_sorter.

---
+++ test/test_i18n_yaml_sorter.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestI18nYamlSorter < Test::Unit::TestCase
+  def test_something_for_real
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
